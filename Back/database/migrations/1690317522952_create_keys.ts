0669a798230219b949d5ba12bf85cd6b69be976d
Victor Henrique <victor@guardadoria.com.br>
2023-07-25 17:14:38 -0400

    feat: configure DB and create Key Table

---
+++ Back/database/migrations/1690317522952_create_keys.ts
@@ -0,0 +1,22 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class CreateKeysTable extends BaseSchema {
+  protected tableName = 'keys'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.uuid('id').primary()
+      table.string('name').notNullable()
+      table.boolean('isAvailable').notNullable()
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}

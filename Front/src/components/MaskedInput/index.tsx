651c89a3bfed728c4f15fd602be5621d7c5b4c00
Victor Lima <victorhenriqu3@outlook.com>
2024-01-24 20:22:57 -0400

    fix: MaskedInput props Error

--- Front/src/components/MaskedInput/index.tsx
+++ Front/src/components/MaskedInput/index.tsx
@@ -1,7 +1,9 @@
 import { IMaskMixin } from 'react-imask';
 import Input, { InputProps } from '../Input';
 
-export type MaskedInputProps = Omit<InputProps, 'onChange'> & any;
+export type MaskedInputProps = Omit<InputProps, 'onChange'> & {
+  inputRef: React.Ref<HTMLInputElement>
+};
 
 const MaskedInput = IMaskMixin((props: MaskedInputProps) => {
   const { inputRef, ...rest } = props;
